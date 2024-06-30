f9c3f1deab5e36ebbed69e563665554f77f192f3
xMatysz <matizek56@gmail.com>
2024-06-30 10:54:14 +0200

    feat: add lambda support for fiszkit.api

--- src/FiszkIt.Api/Program.cs
+++ src/FiszkIt.Api/Program.cs
@@ -1,13 +1,18 @@
 using FiszkIt.Domain;
-using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () =>
 {
-    var x = new Flashcard("HowAreYou?", "Good");
-    return x;
-});
+    builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+}
+
+var app = builder.Build();
+{
+    app.MapGet("/", () =>
+    {
+        var x = new Flashcard("HowAreYou?", "Good");
+        return x;
+    });
+
+}
 
 app.Run();
